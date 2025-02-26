50b50fc9d3624bf65e5802151b4436625312e058
ivan-nesterenko-cgs <ivan.ne@cgsteam.io>
2025-02-26 12:49:21 +0300

    Update global.d.ts

--- global.d.ts
+++ global.d.ts
@@ -7,6 +7,8 @@ declare global {
 
   type PartialRecord<T extends string | number | symbol, K = unknown> = { [key in T]?: K };
 
+type PartialRecord<K extends keyof unknown, V = unknown> = { [key in K]?: V };
+  
 type OR<T, K> =
   | (T & { [P in Exclude<keyof K, keyof T>]?: never })
   | (K & { [P in Exclude<keyof T, keyof K>]?: never });
